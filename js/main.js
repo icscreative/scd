72fad6b31bd56b906b9a8dbb0dc8fe7daf0b0b13
tranzillo <tranzillo.m@gmail.com>
2023-04-18 10:24:21 -0400

    intit  commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function () {
+    function setMenu(element) {
+        navItem = $(element).parent('li')
+        navMenu = navItem.children('section')
+
+        baseREM = $('body').css('font-size');
+        borderPad = parseFloat(baseREM) / 4;
+        console.log(borderPad)
+
+        offsetRight = Math.ceil($(window).width() - (navMenu.offset().left + navMenu.outerWidth()) - borderPad);
+        navMaxHeight = Math.abs(((navMenu.offset().top - $(document).scrollTop() ) - $(window).height()))
+
+        navMenu.css('max-height', navMaxHeight)
+
+        console.log(offsetRight)
+        if (offsetRight < 0) {
+            navMenu.css('left', offsetRight + 'px')
+        }
+    }
+    $(window).bind('load resize', function () {
+        $('nav section').css('left', 'auto')
+        $('nav button').each(function () {
+            setMenu(this)
+        })
+    })
+});
